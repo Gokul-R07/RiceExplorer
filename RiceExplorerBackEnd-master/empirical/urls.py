48cb6417456842e1974b331dd207a8040a98a50e
gokul <gokulramdevan@gmail.com>
2024-03-10 21:14:44 +0530

    files pushed

---
+++ RiceExplorerBackEnd-master/empirical/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'empirical'
+
+urlpatterns = [
+    path('', views.run_algorithm, name="set_params"),
+    path('export', views.handle_export_result, name="export")
+]
